7124db051bc91ce41c5e18943d04ec9982943af4
Sushant Chavan <144045946+SushantChavan1999@users.noreply.github.com>
2025-05-09 16:27:37 +0530

    Add files via upload

---
+++ DB_Creation_MySQL.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS walmart_db;
+
+CREATE DATABASE walmart_db;
+
+USE walmart_db;	
+
+SHOW COLUMNS FROM walmart_sales;
+
+SELECT * FROM walmart_sales;
+
+DROP TABLE walmart_sales;
